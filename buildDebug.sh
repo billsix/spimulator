87b1d140329ff0de6046b371bbf567d58c5bdcd5
William Emerison Six <billsix@gmail.com>
2021-09-05 11:25:21 -0500

    Added tests to the Linux build script

--- buildDebug.sh
+++ buildDebug.sh
@@ -1,3 +1,5 @@
+export MAIN_DIRECTORY=$(pwd)
+
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform
     echo
@@ -5,7 +7,6 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cd CPU/
     flex -I -8 -o lex.yy.c scanner.l
     bison -d --defines=parser_yacc.h --output=parser_yacc.c -p yy parser.y
-    bison -d --defines=../QtSpim/parser_yacc.h --output=../QtSpim/parser_yacc.c -p yy parser.y
     cd ../
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Do something under 32 bits Windows NT platform
@@ -23,5 +24,32 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=../buildInstall  -DCMAKE_BUILD_TYPE=Debug ../
 cmake --build  . --target all
 cmake --build  . --target install
-#cd ../buildInstall
-#./bin/spim
+
+export TEST_DIR=$MAIN_DIRECTORY/Tests/
+export CPU_DIR=$MAIN_DIRECTORY/CPU/
+
+# test
+cd ../buildInstall/bin
+
+echo "Testing tt.bare.s:"
+./spim -delayed_branches -delayed_loads -noexception -file $TEST_DIR/tt.bare.s >& test.out
+tail -2 test.out
+
+echo "Testing tt.core.s:"
+./spim -ef $CPU_DIR/exceptions.s -file $TEST_DIR/tt.core.s < $TEST_DIR/tt.in >& test.out
+tail -2 test.out
+
+echo "Testing tt.le.s:"
+./spim -ef $CPU_DIR/exceptions.s -file $TEST_DIR/tt.le.s  >& test.out
+tail -2 test.out
+
+# The following two tests come from test_bare, and they don't pass on version
+# v9.1.2, so I don't care that they fail the same way in my branch
+
+# echo "Testing tt.alu.bare.s:"
+# ./spim -bare -noexception -file $TEST_DIR/tt.alu.bare.s >& test.out
+# tail -2 test.out
+
+# echo "Testing tt.fpt.bare.s:"
+# ./spim -bare -noexception -file $TEST_DIR/tt.fpu.bare.s >& test.out
+# tail -2 test.out
