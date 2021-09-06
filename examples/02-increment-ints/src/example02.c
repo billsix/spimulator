3a6bb3b41e18353660c7443295dc0f5ad1cb46ef
William Emerison Six <billsix@gmail.com>
2021-09-06 09:49:29 -0500

    reorder the examples

---
+++ examples/02-increment-ints/src/example02.c
@@ -0,0 +1,55 @@
+/* Permission is hereby granted for anyone to do anything that they */
+/* want with this material—you may freely reprint it, redistribute it, */
+/* amend it or do whatever you like with it provided that */
+/* you include an acknowledgement of the original authorship and copyright in */
+/* the form of a link to this page. In doing so */
+/* you must accept that you do so strictly on your own */
+/* liability and that you accept any consequences with no liability whatsoever */
+/* remaining with the original authors. If you find the material useful */
+/* and happen to encounter one of the authors, it is unlikely */
+/* that they will refuse offers to buy them a drink. You */
+/* may therefore like to consider this material ‘drinkware’. (Offer void where */
+/* prohibited by law, in which case fawning and flattery may be substituted.) */
+
+/* https://publications.gbdirect.co.uk//c_book/copyright.html */
+
+
+
+/* Copyright (c) 2021 William Emerison Six */
+
+/* Permission is hereby granted, free of charge, to any person obtaining a copy */
+/* of this software and associated documentation files (the "Software"), to deal */
+/* in the Software without restriction, including without limitation the rights */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell */
+/* copies of the Software, and to permit persons to whom the Software is */
+/* furnished to do so, subject to the following conditions: */
+
+/* The above copyright notice and this permission notice shall be included in all */
+/* copies or substantial portions of the Software. */
+
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE */
+/* SOFTWARE. */
+
+/* Purpose */
+
+/* Define a couple of ints.  Increment them.  Compute addition of various integers. */
+/* Print out the results */
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int32_t a, b;
+  a = b = 5;
+  printf("%d\n", ++a + 5);
+  printf("%d\n", a);
+  printf("%d\n", b++ + 5);
+  printf("%d\n", b);
+  exit(EXIT_SUCCESS);
+}
